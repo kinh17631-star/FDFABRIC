94bcb8a69a6d9f93a2065427074bb0b0f296c688
kinh17631-star <kinh17631@gmail.com>
2026-06-23 21:30:00 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,16 +4,16 @@ import WhatsApp from "@/components/whatsapp";
 
 export default function Home() {
   return (
-    // pt-20 इसलिए ताकि नेविगेशन बार हमारी फोटो के ऊपर न चढ़े
-    <div className="flex flex-col min-h-screen pt-20 relative"> 
+    // pt-20 इसलिए है ताकि आपका Navbar किसी टेक्स्ट को न छुपाए
+    <div className="flex flex-col min-h-screen pt-20 relative bg-gray-900"> 
       
-      {/* 1. सबसे ऊपर वेल्डर वाली सिनेमैटिक फोटो और नाम */}
+      {/* 1. साउथ एक्सटेंशन वाला सिनेमैटिक बैनर */}
       <Hero />
       
-      {/* 2. बीच में ताऊ जी का इंडस्ट्रियल काम */}
+      {/* 2. स्टील गेट और वेल्डिंग का काम */}
       <Services />
       
-      {/* 3. फ्लोटिंग व्हाट्सएप बटन */}
+      {/* 3. सद्दाम भाई का डायरेक्ट व्हाट्सएप */}
       <WhatsApp />
       
     </div>
